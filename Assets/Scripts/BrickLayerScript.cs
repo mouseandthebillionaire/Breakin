a1d5edb2749dc960708f56d66e9e707eb59401e5
mouseandthebillionaire <matt@mouseandthebillionaire.com>
2020-09-09 16:08:02 -0600

    Complete End of Day 1 (Wednesday)

---
+++ Assets/Scripts/BrickLayerScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickLayerScript : MonoBehaviour
+{
+    public Transform brick;
+    public int numRows, numColumns;
+    public float xSpacing, ySpacing;
+    public float xOrigin, yOrigin;
+    public Color[] brickColors;
+    public int[] scorePoints = {10, 20, 30, 40, 50};
+
+    public static BrickLayerScript S;
+
+    void Awake()
+    {
+        S = this;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        LayBricks(numRows, numColumns);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LayBricks(int _numRows, int _numColumns)
+    {
+        for (int i = 0; i < _numRows; i++)
+        {
+            for(int j=0; j< _numColumns; j++)
+            {
+                Transform t = Instantiate(brick);
+                t.transform.parent = this.transform;
+                Vector2 loc = new Vector2(xOrigin + (i * xSpacing), yOrigin - (j * ySpacing));
+
+                SpriteRenderer sr = t.GetComponent<SpriteRenderer>();
+                sr.color = brickColors[j];
+
+                GameManagerScript.S.AddToTotalBricks();
+                t.transform.position = loc;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public float score;
+    public int numBricks;
+    public int lives;
+
+    public int totalBricks;
+    public static GameManagerScript S;
+
+    void Awake()
+    {
+        S = this;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        numBricks = 0;
+        lives = 3;
+        score = 0;
+        DontDestroyOnLoad(this);
+    }
+
+    public void AddBrick()
+    {
+        numBricks++;
+        if (numBricks >= totalBricks)
+        {
+            // You Win
+            BrickLayerScript.S.LayBricks(5, 8);
+
+        }
+        Debug.Log(numBricks);
+    }
+    
+    public void DecreaseLives()
+    {
+        lives--;
+        if (lives < 0) {
+            // Game is Over
+            SceneManager.LoadScene("GameOver")
+        }
+        Debug.Log(lives);
+    }
+
+    public void Scorer(float scoreToAdd)
+    {
+        score += scoreToAdd;
+        Debug.Log("Score: " + score);
+    }
+
+    public void AddToTotalBricks()
+    {
+        totalBricks++;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
